e14dc127e80945dadee11f801e85e138e60618b6
Wonyeong Choi <coma01155@gmail.com>
2021-07-19 01:58:07 +0900

    feat: add resize event listener

--- src/view/GIF-Maker/StreamPreview.jsx
+++ src/view/GIF-Maker/StreamPreview.jsx
@@ -1,8 +1,25 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 // eslint-disable-next-line react/prop-types
 const StreamPreview = ({ stream }) => {
+  const [currentWidth, setCurrentWidth] = useState(window.innerWidth / 2);
+  const [currentHeight, setCurrentHeight] = useState(window.innerHeight / 2);
+
   const preview = useRef();
+
+  useEffect(() => {
+    const updateWindowDimensions = () => {
+      const newWidth = window.innerWidth / 2;
+      const newHeight = window.innerHeight / 2;
+      setCurrentWidth(newWidth);
+      setCurrentHeight(newHeight);
+    };
+
+    window.addEventListener('resize', updateWindowDimensions);
+    return () => {
+      window.removeEventListener('resize', updateWindowDimensions);
+    };
+  }, []);
 
   useEffect(() => {
     if (preview.current && stream) {
@@ -10,11 +27,12 @@ const StreamPreview = ({ stream }) => {
     }
   }, [stream]);
 
-  if (!stream) {
-    return <div style={{ width: 1520, height: 680 }} />;
-  }
-  // eslint-disable-next-line jsx-a11y/media-has-caption
-  return <video ref={preview} width={1520} height={680} autoPlay />;
+  return (
+    { stream }
+      // eslint-disable-next-line jsx-a11y/media-has-caption
+      ? <video ref={preview} width={currentWidth} height={currentHeight} autoPlay style={{ padding: 'auto' }} />
+      : <div style={{ width: currentWidth, height: currentHeight, padding: 'auto' }} />
+  );
 };
 
 export default StreamPreview;
