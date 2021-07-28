8545067e66c13ff2681f1aba17be1268b930b799
Wonyeong Choi <coma01155@gmail.com>
2021-07-29 00:27:27 +0900

    feat: COEP, COOP 설정 완료 ^--^

---
+++ config-overrides.js
@@ -0,0 +1,14 @@
+module.exports = {
+  devServer(configFunction) {
+    return function (proxy, allowedHost) {
+      const config = configFunction(proxy, allowedHost);
+
+      config.headers = {
+        'Cross-Origin-Embedder-Policy': 'require-corp',
+        'Cross-Origin-Opener-Policy': 'same-origin',
+      };
+
+      return config;
+    };
+  },
+};
